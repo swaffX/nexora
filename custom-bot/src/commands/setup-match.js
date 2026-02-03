49c94c68a86db976980bbdec8f5d34ddf3bc9f5e
swaffX <kxrk0@github.com>
2026-02-03 21:39:04 +0300

    aefafaef

--- custom-bot/src/commands/setup-match.js
+++ custom-bot/src/commands/setup-match.js
@@ -24,7 +24,7 @@ module.exports = {
             .setColor(0x2F3136) // Dark Theme Background
             .setAuthor({ name: `NEXORA COMPETITIVE • ${lobbyConfig.name}`, iconURL: 'https://cdn.discordapp.com/emojis/1467546027518197915.webp?size=96&quality=lossless' })
             .setDescription(`## <:valo:1468313683469013206> ARENAYA HOŞ GELDİN <a:tacticbear:1467545426009002055>\n\nTakımını topla, stratejini belirle ve mücadeleye başla.\nOdanı kurmak için aşağıdaki butonu kullan.\n\n> <a:jetto:1467545477221318750> **Dikkat:** Odamızı kurmadan önce **<#${lobbyConfig.voiceId}>** ses kanalına giriş yapınız.`)
-            .setImage('https://i.pinimg.com/originals/a0/a6/50/a0a6504d603e87483ee76059fc7100e4.gif') // Şık, minimalist bir Valorant Gif'i
+            .setImage('https://cdn.discordapp.com/attachments/531892263652032522/1464235225818075147/standard_2.gif?ex=69833b52&is=6981e9d2&hm=c0f38e95cf39afdabbfa335c87c9b85ee19be62255a529c26e2605bccf9459d7&')
             .setFooter({ text: 'Nexora Systems' });
 
         const row = new ActionRowBuilder().addComponents(
